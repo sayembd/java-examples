9100f1d7f74c3fa2dfa547e01a611e76d744779b
MD Sayem Ahmed <contact@sayemahmed.com>
2020-02-22 20:31:26 +0100

    Add spring boot example

---
+++ spring-boot-example/src/main/java/com/sayemahmed/example/springboot/SpringBootExampleApplication.java
@@ -0,0 +1,13 @@
+package com.sayemahmed.example.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExampleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootExampleApplication.class, args);
+  }
+
+}
---
+++ spring-boot-example/src/test/java/com/sayemahmed/example/springboot/SpringBootExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sayemahmed.example.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootExampleApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
